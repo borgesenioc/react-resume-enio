7530909170a445356ae712810fe17586847859b7
borgesenioc <borgesenioc@gmail.com>
2025-02-02 23:45:32 -0300

    Update footer color

--- src/components/Sections/Footer.tsx
+++ src/components/Sections/Footer.tsx
@@ -7,19 +7,19 @@ import Socials from '../Socials';
 const currentYear = new Date().getFullYear();
 
 const Footer: FC = memo(() => (
-  <div className="relative bg-neutral-900 px-4 pb-6 pt-12 sm:px-8 sm:pb-8 sm:pt-14">
+  <div className="relative bg-white px-4 pb-6 pt-12 sm:px-8 sm:pb-8 sm:pt-14">
     <div className="absolute inset-x-0 -top-4 flex justify-center sm:-top-6">
       <a
-        className="rounded-full bg-gray-800 p-1 ring-white ring-offset-2 ring-offset-gray-700/80 focus:outline-none focus:ring-2 sm:p-2"
+        className="rounded-full bg-gray-700 p-1 ring-white ring-offset-2 ring-offset-gray-700/80 focus:outline-none focus:ring-2 sm:p-2"
         href={`/#${SectionId.Hero}`}>
-        <ChevronUpIcon className="h-6 w-6 text-white bg-gray-800 sm:h-8 sm:w-8" />
+        <ChevronUpIcon className="h-6 w-6 text-white bg-gray-700 sm:h-8 sm:w-8" />
       </a>
     </div>
     <div className="flex flex-col items-center gap-y-6">
-      <div className="flex gap-x-4 text-neutral-500">
+      <div className="flex gap-x-4 text-black">
         <Socials />
       </div>
-      <span className="text-sm text-white">© Copyright {currentYear} Enio Borges</span>
+      <span className="text-sm text-gray-900">© Copyright {currentYear} Enio Borges</span>
     </div>
   </div>
 ));
