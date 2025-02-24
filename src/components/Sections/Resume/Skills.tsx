3b2362ce80d62919914ba76692ce451cace23f55
Enio Borges <borgesenioc@gmail.com>
2025-02-24 19:49:43 -0300

    Adjust skill styling to pills not percentages

--- src/components/Sections/Resume/Skills.tsx
+++ src/components/Sections/Resume/Skills.tsx
@@ -19,15 +19,11 @@ export const SkillGroup: FC<PropsWithChildren<{skillGroup: SkillGroupType}>> = m
 SkillGroup.displayName = 'SkillGroup';
 
 export const Skill: FC<{skill: SkillType}> = memo(({skill}) => {
-  const {name, level, max = 10} = skill;
-  const percentage = useMemo(() => Math.round((level / max) * 100), [level, max]);
-
   return (
-    <div className="flex flex-col">
-      <span className="ml-2 text-sm font-medium">{name}</span>
-      <div className="h-5 w-full overflow-hidden rounded-full bg-neutral-300">
-        <div className="h-full rounded-full bg-orange-400" style={{width: `${percentage}%`}} />
-      </div>
+    <div className="inline-block overflow-hidden rounded-full bg-orange-400">
+      <span className="px-4 py-2 text-lg font-bold text-white">
+        {skill.name}
+      </span>
     </div>
   );
 });
